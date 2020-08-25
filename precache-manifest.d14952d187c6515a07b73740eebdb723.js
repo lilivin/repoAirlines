fff3b56e9cc83582c842b400688e5bc37aaea9c2
KLiwin <k.liwinski@gmail.com>
2020-08-25 11:47:04 +0200

    Updates

---
+++ precache-manifest.d14952d187c6515a07b73740eebdb723.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989f57f849c8b9bd90a9eb1c3f07aacd",
+    "url": "/repoAirlines/index.html"
+  },
+  {
+    "revision": "80ef5bf21d2449eb9321",
+    "url": "/repoAirlines/static/css/main.5b4754a3.chunk.css"
+  },
+  {
+    "revision": "9e3c635d2773c19edfba",
+    "url": "/repoAirlines/static/js/2.487bf461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repoAirlines/static/js/2.487bf461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ef5bf21d2449eb9321",
+    "url": "/repoAirlines/static/js/main.c71b1381.chunk.js"
+  },
+  {
+    "revision": "582f91babaf9e94b23ca",
+    "url": "/repoAirlines/static/js/runtime-main.aaec531d.js"
+  },
+  {
+    "revision": "927dded9a3fccf9f16b22f06c9203ddc",
+    "url": "/repoAirlines/static/media/ateny.927dded9.jpg"
+  },
+  {
+    "revision": "1a6c2585defb79e117ed14782a5a753c",
+    "url": "/repoAirlines/static/media/budapest.1a6c2585.jpg"
+  },
+  {
+    "revision": "cb121f378929520fc2ef204d7661367f",
+    "url": "/repoAirlines/static/media/lizbona.cb121f37.jpg"
+  },
+  {
+    "revision": "092603066efbd89f6abc6c8ee71aa540",
+    "url": "/repoAirlines/static/media/mainPhoto.09260306.jpg"
+  }
+]);
